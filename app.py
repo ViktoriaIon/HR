678212e616bf55964492fe05ed919573feffbb26
dannade <Kandinskaya.nadezhda@gmail.com>
2026-02-18 15:59:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
-app = Flask(__name__)
+RECORDS = [
+    {"id": 1, "full_name": "Анна Иванова", "date": "25.02.2026 10:00"},
+    {"id": 2, "full_name": "Олег Петров", "date": "25.02.2026 15:00"},
+    {"id": 3, "full_name": "Мария Смирнова", "date": "26.02.2026 10:00"},
+    {"id": 4, "full_name": "Олег Олегов", "date": "27.02.2026 12:00"}
+]
+
 @app.route('/')
-def hello():
-  name='Девчули'
-  return render_template('index.html', name=name)
+def hello_world():
+    hello='Hello, World!'
+    return render_template("index.html", hello=hello)
+
+@app.route("/list_records", methods=['GET', 'POST'])
+def list_records(): 
+    if request.method == 'POST':
+        # удаляем последний элемент, если есть
+        if RECORDS:
+            RECORDS.pop()
+        # перенаправляем, чтобы избежать повторной отправки формы при обновлении
+        return redirect(url_for('list_records'))
+    return render_template("list_records.html", records=RECORDS)
 
 if __name__ == '__main__':
-  app.run(debug=True)
+    app.run(debug=True)
