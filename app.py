8999708289292278bc2e8f23c114c8430de9eec6
enngereles-svg <enn.gereles@gmail.com>
2026-02-18 11:54:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
+
 @app.route('/')
 def hello_world():
   return 'ля красиво'
+
+@app.route('/records')
+def records():
+  return 'Our records'
 if __name__ == '__main__':
   app.run(debug=True)
   
