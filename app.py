79e77c69c151e5c8c8ab042a508482408e480b94
dannade <Kandinskaya.nadezhda@gmail.com>
2026-02-18 10:59:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask
+
 app = Flask(__name__)
-@app.route('/')
+
+
+@app.route("/")
 def hello_world():
-  return 'ля красиво'
-if __name__ == '__main__':
-  app.run(debug=True)
-  
+    return "ля красиво"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
