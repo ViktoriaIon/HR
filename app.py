d730c25672f673156c724f9a901dee4c5d02b29e
ViktoriaIon <viktorina_ion@inbox.ru>
2026-02-18 13:51:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+app = Flask(__name__)
 @app.route('/')
-def hello_world():
-  return 'ля красиво'
-
-@app.route('/records')
-def records():
-  return 'Our records'
+def hello():
+  name='Девчули'
+  return render_template('index.html', name=name)
 
 if __name__ == '__main__':
   app.run(debug=True)
-  
