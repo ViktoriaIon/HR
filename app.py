a0b1d15336462d84501c1924b9f7f42db460f650
dannade <Kandinskaya.nadezhda@gmail.com>
2026-02-24 10:14:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,41 @@
 from flask import Flask, render_template, request, redirect, url_for
+import pandas as pd
+
 app = Flask(__name__)
 
-RECORDS = [
-    {"id": 1, "full_name": "Анна Иванова", "date": "25.02.2026 10:00"},
-    {"id": 2, "full_name": "Олег Петров", "date": "25.02.2026 15:00"},
-    {"id": 3, "full_name": "Мария Смирнова", "date": "26.02.2026 10:00"},
-    {"id": 4, "full_name": "Олег Олегов", "date": "27.02.2026 12:00"}
-]
 
-@app.route('/')
-def hello_world():
-    hello='Hello, World!'
-    return render_template("index.html", hello=hello)
+CONSULT = {1: "Анна Иванова", 2: "Олег Петров", 3: "Мария Сидорова"}
 
-@app.route("/list_records", methods=['GET', 'POST'])
-def list_records(): 
-    if request.method == 'POST':
-        # удаляем последний элемент, если есть
-        if RECORDS:
-            RECORDS.pop()
-        # перенаправляем, чтобы избежать повторной отправки формы при обновлении
-        return redirect(url_for('list_records'))
-    return render_template("list_records.html", records=RECORDS)
 
-if __name__ == '__main__':
+CONSULT1 = {
+    1: "Консультант Анна чудесно работает с выгоранием",
+    2: "Помогает победить прокрастинацию и начать работать",
+    3: "Выслушивает жалобы на руководство",
+}
+
+
+@app.route("/")
+def consult():
+    return render_template("index.html", buttons=CONSULT)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+@app.route("/")
+def consult1():
+    return render_template("index.html", text=CONSULT1)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+@app.route("/")
+def cons1():
+    return render_template("cons1.html")
+
+
+if __name__ == "__main__":
     app.run(debug=True)
